180972cb4363986e371bd550a7d871385d7b67b6
elijahsamuels <elijahsamuels@gmail.com>
2022-03-17 11:21:12 -0600

    changed development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,10 +76,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: "",
-    password: "",
-    # user_name: ENV['GMAIL_USERNAME'],
-    # password: ENV['GMAIL_PASSWORD'],
+    # user_name: "",
+    # password: "",
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
 }
